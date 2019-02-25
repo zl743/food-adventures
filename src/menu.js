a25a7d6552c506a3acefd4c4694110a389116a83
Zach Lee <zach@Alvins-Air.fios-router.home>
2019-02-24 21:30:52 -0500

    food updates

--- src/menu.js
+++ src/menu.js
@@ -3,12 +3,25 @@ import data from './data/food.json';
 
 function Menu(props) {
   return (
-    <div className="col red darken-3 s12 m4 fullRowContent z-depth-3">
+    <div>
+    <div className="col red accent-4 s12 m4 navbar-fixed valign-wrapper z-depth-3">
+    <ul class="tabs tabs-transparent">
+        <li class="tab"><a>Date</a></li>
+        <li class="tab"><a>A-Z</a></li>
+        <li class="tab"><a>Restaurants</a></li>
+        <li class="tab"><a>Recipes</a></li>
+      </ul>
+    </div>
+    <div className="col red lighten-1 s12 m4 fullRowContent z-depth-3">
+
     {/* card html*/}
     {data.sort(function(a,b){
       var keyA = a.id;
       var keyB = b.id;
-      console.log(keyA, keyB)
+      //console.log(keyA, keyB);
+      if(keyA < keyB) return -1;
+      if(keyA > keyB) return 1;
+      return 0;
     }).map(card => { return(
       <div className="col s12" key={card.id}>
         <div className={card.type == 'restaurant' ? 'card horizontal' : 'card horizontal recipe amber lighten-5'}>
@@ -28,6 +41,7 @@ function Menu(props) {
       </div>
     )})}
     {/*end card html*/}
+  </div>
   </div>
   );
 }
